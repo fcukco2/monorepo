ebf3e701c8c1669e766f52773bff05db1816d971
petie <piotr.korgol@gmail.com>
2023-06-11 00:44:48 +0200

    Attach smart contract

--- frontend/src/components/LandingPage/Header.js
+++ frontend/src/components/LandingPage/Header.js
@@ -1,10 +1,10 @@
 import * as React from "react";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import ConnectWallet from "../ConnectWallet";
 import { ethers } from "ethers";
 import CO2Burner from "../../contracts/CO2Burner.json";
 import contractAddress from "../../contracts/contract-address.json";
+import usdc from "../../contracts/usdc.json";
 import AppBar from "@mui/material/AppBar";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
@@ -17,16 +17,9 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 
-// The next two methods are needed to start and stop polling data. While
-// the data being polled here is specific to this example, you can use this
-// pattern to read any data from your contracts.
-//
-// Note that if you don't need it to update in near real time, you probably
-// don't need to poll it. If that's the case, you can just fetch it when you
-// initialize the app, as we do with the token data.
 const HARDHAT_NETWORK_ID = "80001";
 
 export class Header extends React.Component {
@@ -34,12 +27,9 @@ export class Header extends React.Component {
     super(props);
     this.title = props.title;
     this.initialState = {
-      // The info of the token (i.e. It's Name and symbol)
       tokenData: undefined,
-      // The user's address and balance
       selectedAddress: undefined,
       balance: undefined,
-      // The ID about transactions being sent, and any possible error with them
       txBeingSent: undefined,
       transactionError: undefined,
       networkError: undefined,
@@ -54,31 +44,24 @@ export class Header extends React.Component {
   }
 
   _initialize(userAddress) {
-    // This method initializes the dapp
-
-    // We first store the user's address in the component's state
     this.setState({
       selectedAddress: userAddress,
     });
-
-    // Then, we initialize ethers, fetch the token's data, and start polling
-    // for the user's balance.
-
-    // Fetching the token data and the user's balance are specific to this
-    // sample project, but you can reuse the same initialization pattern.
     this._initializeEthers();
     this._startPollingData();
   }
 
   async _initializeEthers() {
-    // We first initialize ethers by creating a provider using window.ethereum
     this._provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    // Then, we initialize the contract using that provider and the token's
-    // artifact. You can do this same thing with your contracts.
     this.co2Burner = new ethers.Contract(
       contractAddress.CO2Burner,
       CO2Burner.abi,
+      this._provider.getSigner(0)
+    );
+
+    this.usdc = new ethers.Contract(
+      "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+      usdc,
       this._provider.getSigner(0)
     );
 
@@ -86,29 +69,13 @@ export class Header extends React.Component {
   }
 
   async _connectWallet() {
-    // This method is run when the user clicks the Connect. It connects the
-    // dapp to the user's wallet, and initializes it.
-
-    // To connect to the user's wallet, we have to run this method.
-    // It returns a promise that will resolve to the user's address.
     const [selectedAddress] = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
-
-    // Once we have the address, we can initialize the application.
-
-    // First we check the network
     this._checkNetwork();
-
     this._initialize(selectedAddress);
-
-    // We reinitialize it whenever the user changes their account.
     window.ethereum.on("accountsChanged", ([newAddress]) => {
       this._stopPollingData();
-      // `accountsChanged` event can be triggered with an undefined newAddress.
-      // This happens when the user removes the Dapp from the "Connected
-      // list of sites allowed access to your addresses" (Metamask > Settings > Connections)
-      // To avoid errors, we reset the dapp state
       if (newAddress === undefined) {
         return this._resetState();
       }
@@ -124,8 +91,6 @@ export class Header extends React.Component {
 
   _startPollingData() {
     this._pollDataInterval = setInterval(() => this._updateBalance(), 1000);
-
-    // We run it once immediately so we don't have to wait for it
     this._updateBalance();
   }
 
@@ -143,7 +108,6 @@ export class Header extends React.Component {
     await this._initialize(this.state.selectedAddress);
   }
 
-  // This method checks if the selected network is Localhost:8545
   _checkNetwork() {
     if (window.ethereum.networkVersion !== HARDHAT_NETWORK_ID) {
       this._switchChain();
@@ -166,6 +130,30 @@ export class Header extends React.Component {
     this.setState({ countryFilter: event.target.value });
   }
 
+  async handleRetire() {
+    await this._connectWallet();
+    const amountEth = this.state.amount * 10 ** 6;
+    const approvedAmount = await this.usdc.allowance(
+      this.selectedAddress,
+      contractAddress.CO2Burner
+    );
+    if (approvedAmount < amountEth) {
+      const approval = await this.usdc.approve(
+        contractAddress.CO2Burner,
+        amountEth
+      );
+      await approval.wait();
+      console.log("Approval tx hash: " + approval.hash);
+    }
+    const tx = await this.co2Burner.burnCO2(
+      this.state.amount,
+      this.state.countryFilter + this.state.categoryFilter
+    );
+    await tx.wait();
+    console.log("Burner tx hash: " + tx.hash);
+    this.setState({ open: false });
+  }
+
   render() {
     return (
       <AppBar
@@ -175,8 +163,16 @@ export class Header extends React.Component {
       >
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-            <Grid container justifyContent="flex-start" alignItems="center" style={{paddingLeft: '24px'}}>
-              <img src={'/co2terminator.png'} style={{width: '55px', marginRight: '10px'}}/>
+            <Grid
+              container
+              justifyContent="flex-start"
+              alignItems="center"
+              style={{ paddingLeft: "24px" }}
+            >
+              <img
+                src={"/co2terminator.png"}
+                style={{ width: "55px", marginRight: "10px" }}
+              />
               <Typography
                 variant="h6"
                 noWrap
@@ -195,7 +191,12 @@ export class Header extends React.Component {
               </Typography>
             </Grid>
             <Grid container justifyContent="flex-end">
-              <Button onClick={() => this.handleClickOpen()}>Retire</Button>
+              <Button
+                variant="contained"
+                onClick={() => this.handleClickOpen()}
+              >
+                Retire
+              </Button>
             </Grid>
           </Toolbar>
         </Container>
@@ -256,7 +257,7 @@ export class Header extends React.Component {
           </DialogContent>
           <DialogActions>
             <Button onClick={() => this.handleClose()}>Cancel</Button>
-            <Button variant="contained" onClick={() => this.handleClose()}>
+            <Button variant="contained" onClick={() => this.handleRetire()}>
               Retire
             </Button>
           </DialogActions>
